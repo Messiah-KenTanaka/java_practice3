1ef0808af676a899b683a5af0698009b6bc90493
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2021-12-02 17:36:05 +0900

    最初のコミット

---
+++ QuickMaster1/src/main/java/to/msn/wings/quickmaster/QuickMaster1Application.java
@@ -0,0 +1,13 @@
+package to.msn.wings.quickmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickMaster1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickMaster1Application.class, args);
+	}
+
+}
