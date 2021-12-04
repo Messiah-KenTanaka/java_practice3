7d66977a744db7dd879c32507fb2d878df7779a7
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2021-12-04 12:33:43 +0900

    フラグメントを追加

---
+++ QuickMaster2/src/main/java/to/msn/wings/quickmaster/HelloController.java
@@ -0,0 +1,38 @@
+package to.msn.wings.quickmaster;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import to.msn.wings.quickmaster.model.BookRepository;
+
+@Controller
+public class HelloController {
+
+	private BookRepository rep;
+
+	@Autowired
+	public HelloController(BookRepository rep) {
+		this.rep = rep;
+	}
+
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("books", rep.findAll());
+		return "list";
+	}
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String index() {
+		return "こんにちは、世界！";
+	}
+
+	@GetMapping("greet")
+	public String greet(Model model) {
+		model.addAttribute("message", "こんにちは、世界！");
+		return "greet";
+	}
+}
---
+++ QuickMaster2/src/main/java/to/msn/wings/quickmaster/QuickMaster2Application.java
@@ -0,0 +1,13 @@
+package to.msn.wings.quickmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickMaster2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickMaster2Application.class, args);
+	}
+
+}
