dc37fb750febd28df9ea8a5d752b1417fb5d6b8d
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2021-12-05 22:56:14 +0900

    ファイルのアップロードしたよ

--- QuickMaster2/src/main/java/to/msn/wings/quickmaster/controller/CtrlController.java
+++ QuickMaster2/src/main/java/to/msn/wings/quickmaster/controller/CtrlController.java
@@ -1,30 +1,52 @@
 package to.msn.wings.quickmaster.controller;
 
-import java.time.LocalDate;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import to.msn.wings.quickmaster.form.MemberForm;
 
 @Controller
 public class CtrlController {
 
 	@GetMapping("/form")
-	public String form(Model model) {
+	public String form(@ModelAttribute MemberForm memberForm, Model model) {
 		model.addAttribute("main", "ctrl/form::main");
 		return "common/layout";
 	}
 
 	@PostMapping("/ctrl/form")
-	public String form(@RequestParam String name, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate birth,
-			@RequestParam String email, Model model) {
-		model.addAttribute("name", name);
-		model.addAttribute("birth", birth);
-		model.addAttribute("email", email);
+	public String form_result(@ModelAttribute MemberForm memberForm, Model model) {
 		model.addAttribute("main", "ctrl/form_result::main");
+		return "common/layout";
+	}
+
+	@GetMapping("/upload")
+	public String upload(Model model) {
+		model.addAttribute("main", "ctrl/upload::main");
+		return "common/layout";
+	}
+
+	@PostMapping("/ctrl/upload")
+	public String upload(Model model, @RequestParam("upfile") MultipartFile file) {
+		String name = file.getOriginalFilename();
+		// 指定ファイルに保存
+		try (BufferedOutputStream bof = new BufferedOutputStream(new FileOutputStream("./" + name))) {
+			bof.write(file.getBytes());
+			model.addAttribute("success", name + "のアップロードに成功しました");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// フォームを再描画
+		model.addAttribute("main", "ctrl/upload::main");
 		return "common/layout";
 	}
 }
---
+++ QuickMaster2/src/main/java/to/msn/wings/quickmaster/form/MemberForm.java
@@ -0,0 +1,20 @@
+package to.msn.wings.quickmaster.form;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+
+	private String name;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate birth;
+
+	private String email;
+}
